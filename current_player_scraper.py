178d3ba555ea228ae38f57492e3283315de90eb9
Eric Ma <amcire96@gmail.com>
2017-03-08 13:37:23 -0500

    added some checks to see which parts aren't working

--- current_player_scraper.py
+++ current_player_scraper.py
@@ -2,11 +2,20 @@ from bs4 import BeautifulSoup
 import requests
 import re
 import json
+import sys
 
 # All years in which draft has already happened and they have player descriptions
 # for year in range(1994, 2017):
 
+
+sys.stdout = open("out.text", "w")
+
+
 player_to_docs = {}
+
+invalid_names = []
+blank_descriptions = []
+
 
 # Just start with years 2005-2016
 for year in range(2005, 2017):
@@ -28,11 +37,19 @@ for year in range(2005, 2017):
             profile_urls.append(possible_player["href"])
     print(profile_urls)
 
+
     for profile_url in profile_urls:
     # for profile_url in [profile_urls[0]]:
         player_name = re.findall(r"\/profile\/([^\/]+)", profile_url)[0]
-        player_name = " ".join(re.split(r"-", player_name)[:-1])
+
+        split_player_name = re.split(r"-", player_name)[:-1]
+
+        if len(split_player_name) == 0 or len(split_player_name) == 1:
+            invalid_names.append(profile_url)
+
+        player_name = " ".join(split_player_name)
         print(player_name)
+
 
         full_profile_url = "http://www.draftexpress.com" + str(profile_url)
         profile_r = requests.get(full_profile_url)
@@ -48,8 +65,20 @@ for year in range(2005, 2017):
             if p.parent.name == "div" and "item" in p.parent["class"]:
                 text.append(p.get_text())
 
+        if len(text) == 0:
+            blank_descriptions.append(profile_url)
+
         print(text)
         player_to_docs[player_name] = text
 
 with open("player_to_docs.json", "w") as f:
     json.dump(player_to_docs, f)
+
+with open("invalid_names.json", "w") as f:
+    json.dump(invalid_names, f)
+
+with open("blank_descriptions.json", "w") as f:
+    json.dump(blank_descriptions, f)
+
+
+sys.stdout.close()
